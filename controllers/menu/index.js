c0a501b834a146c0882b8a81ef0660ad3a138b3b
JuliaGolban <julia.j.shcherban@gmail.com>
2023-09-21 20:04:07 +0300

    add admin

--- controllers/menu/index.js
+++ controllers/menu/index.js
@@ -1,10 +1,12 @@
-const getMenu = require("./getAll");
-const updateMenu = require("./updateMenu");
-const deleteMenu = require("./delete");
-const createMenu = require("./createMenu");
+const getMenu = require('./getAll');
+const getMenuById = require('./getId');
+const updateMenu = require('./updateMenu');
+const deleteMenu = require('./delete');
+const createMenu = require('./createMenu');
 
 module.exports = {
   getMenu,
+  getMenuById,
   updateMenu,
   deleteMenu,
   createMenu,
