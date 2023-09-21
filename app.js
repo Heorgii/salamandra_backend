457343ddd1d5e4240c8b17fb1d983808bf8735b6
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-21 08:57:10 +0300

    add MD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const { errorHandler } = require('./helpers');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const { errorHandler } = require("./helpers");
 const {
   routerAuth,
   routerUser,
@@ -11,30 +11,29 @@ const {
   routerMessage,
   routerAdmin,
   routerDevelopers,
-} = require('./routes/api');
+} = require("./routes/api");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api/auth', routerAuth);
-app.use('/api/menu', routerMenu);
-
+app.use("/api/auth", routerAuth);
+app.use("/api/menu", routerMenu);
 
 app.use((req, res) => {
-  console.log('!!!!! APP (req, res) !!!!!!');
-  res.status(404); // .json({ message: "Not found", data: null });
-  res.json({ messages: 'ERRR JSONS' });
+  console.log("!!!!! APP (req, res) !!!!!!");
+  res.status(404);
+  res.json({ messages: "ERRR JSONS" });
 });
 
 app.use(errorHandler);
