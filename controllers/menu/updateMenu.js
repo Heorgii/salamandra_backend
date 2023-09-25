7dc35edf69b7a378b4da841bf42b76cdfa4fc269
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-25 21:41:15 +0300

    arra for details

--- controllers/menu/updateMenu.js
+++ controllers/menu/updateMenu.js
@@ -9,6 +9,12 @@ const updateMenu = async (req, res, next) => {
   req.file?.path
     ? (newData.images = path.basename(req.file?.path))
     : (newData.images = path.basename("none"));
+  if (newData.details) {
+    newData.details = newData.details.split(",");
+  }
+  if (newData.alcohol) {
+    newData.alcohol = newData.alcohol.split(",");
+  }
   try {
     const resUpdate = await Menu.findByIdAndUpdate({ _id: id }, newData, {
       new: true,
