0bf926382fb8ae88c4cc1ecf46e9f9324f3b3765
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-28 14:00:25 +0300

    images-fix

--- controllers/menu/updateMenu.js
+++ controllers/menu/updateMenu.js
@@ -6,15 +6,7 @@ let path = require("path");
 const updateMenu = async (req, res, next) => {
   const { id } = req.params;
   const newData = dataFilterObj(req.body);
-  req.file?.path
-    ? (newData.images = path.basename(req.file?.path))
-    : (newData.images = path.basename("none"));
-  if (newData.details) {
-    newData.details = newData.details.split(",");
-  }
-  if (newData.alcohol) {
-    newData.alcohol = newData.alcohol.split(",");
-  }
+  if(req.file?.path){newData.images = path.basename(req.file?.path)};
   try {
     const resUpdate = await Menu.findByIdAndUpdate({ _id: id }, newData, {
       new: true,
