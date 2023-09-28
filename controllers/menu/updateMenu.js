de73403f67b7c38f98e2748d600dea3641ed1d1f
JuliaGolban <julia.j.shcherban@gmail.com>
2023-09-28 17:20:51 +0300

    fix edit

--- controllers/menu/updateMenu.js
+++ controllers/menu/updateMenu.js
@@ -1,12 +1,22 @@
-const { ValidationError } = require("../../helpers");
-const { Menu } = require("../../models");
-const { dataFilterObj } = require("../../helpers");
-let path = require("path");
+const { ValidationError } = require('../../helpers');
+const { Menu } = require('../../models');
+const { dataFilterObj } = require('../../helpers');
+let path = require('path');
 
 const updateMenu = async (req, res, next) => {
   const { id } = req.params;
   const newData = dataFilterObj(req.body);
-  if(req.file?.path){newData.images = path.basename(req.file?.path)};
+  if (req.file?.path) {
+    newData.images = path.basename(req.file?.path);
+  }
+  if (!newData.details) {
+    newData.details = [];
+  }
+  if (!newData.alcohol) {
+    newData.alcohol = [];
+  }
+
+  console.log('updateMenu ~ newData:', newData);
   try {
     const resUpdate = await Menu.findByIdAndUpdate({ _id: id }, newData, {
       new: true,
